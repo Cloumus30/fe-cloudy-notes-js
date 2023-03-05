02229e5e52c7d679285ae36aab6daaad1c9f5911
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-03-05 14:50:51 +0700

    Add Search Note

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import { defineComponent } from 'vue';
 import NoteCard from '../components/partial/NoteCard.vue';
 import store from '../store';
 import PopModal from '../components/partial/PopModal.vue';
+import IconSearch from '../components/icons/IconSearch.vue';
 
 export default defineComponent({
   components:{
@@ -14,7 +15,8 @@ export default defineComponent({
     Navbar,
     Navside,
     NoteCard,
-    PopModal
+    PopModal,
+    IconSearch,
 },
 
   mounted(){
@@ -72,7 +74,17 @@ export default defineComponent({
       
       <Navside :class="[navSideActive ? activeNavsideClass : defaultNavsideClass]" />
       
-      <div class="w-5/6 md:w-4/6 xl:w-5/6 h-fit p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gird gap-4 place-items-stretch">
+      <div class="w-5/6 md:w-4/6 xl:w-5/6 h-fit p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-items-stretch">
+        <!-- Search Input -->
+        <div v-if="!navSideActive" class="col-span-1 sm:col-span-2 md:col-span-3">
+          <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <IconSearch />
+            </div>
+            <input type="text" id="search" class="bg-gray-800 border border-gray-200 text-slate-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 " placeholder="Find By Your Title">
+          </div>
+        </div>
+        <!-- Search Input -->
         <NoteCard :title="item.title" :body="body" :noteId="item.id" @deleteEvent="showPopDelete" class=" w-full" v-for="item in listNotes"/>
       </div>
       

9ce05888d408e2cc329cb172fb0ce7cc8127cf6d
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-03-21 07:23:25 +0700

    Add Login with firebase auth google

---
+++ src/components/FirebaseAuthUi.vue
@@ -0,0 +1,27 @@
+<script>
+import { defineComponent } from 'vue';
+import * as firebaseui from 'firebaseui'; 
+import 'firebaseui/dist/firebaseui.css'
+
+export default defineComponent({
+    inject:['firebase'],
+
+    mounted(){
+        let ui = new firebaseui.auth.AuthUI(this.firebase.auth());
+        const firebaseUiConfig = {
+            signInOptions : [
+                this.firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            ],
+            signInFlow: 'popup',
+            signInSuccessUrl: '/login',
+        }
+
+        ui.start('#firebase-auth-container', firebaseUiConfig);
+    }
+})
+
+</script>
+
+<template>
+    <section id="firebase-auth-container"></section>
+</template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,10 @@
-<script lang="ts">
+<script>
 import store from '../store';
 import { defineComponent } from 'vue';
 import { mapActions } from 'vuex';
 import * as yup from 'yup';
 import {Form, Field, ErrorMessage} from 'vee-validate';
+import FirebaseAuthUi from '../components/FirebaseAuthUi.vue';
 
 const schemValidate = yup.object({
     email: yup.string().email().required(),
@@ -15,6 +16,7 @@ export default defineComponent({
     Form,
     Field,
     ErrorMessage,
+    FirebaseAuthUi,
   },
 
   mounted(){
@@ -81,14 +83,14 @@ export default defineComponent({
                     <button type="button" class=" hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-mono rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login Google</button>
                 </div>
 
+                <FirebaseAuthUi />
+
                 <div class="relative z-0 w-full mt-6">
                     <p class="ml-2 text-sm font-medium text-font-navbar">Belum Punya Akun? Silahkan <a href="/register" class="text-white hover:underline"> Daftar </a></p>
                 </div>
-            
-                
             </Form>
         </div>
-
+        
     </div>
     
 </template>

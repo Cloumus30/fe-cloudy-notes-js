2726ae4bab9929317c40afc09f2c12cb6904a039
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-03-22 15:24:06 +0700

    Change axios base URL

--- src/plugin/axios_service.js
+++ src/plugin/axios_service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    // baseURL: 'http://api.cloumus30.online',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://api.cloumus30.online',
+    // baseURL: 'http://localhost:3000',
 });
 
 export const axiosHeaders = () => {
